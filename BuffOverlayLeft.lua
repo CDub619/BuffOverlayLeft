4f7f9e325c6da25d5ad28caea093dd5370558bc7
cdub619 <chriswilliamson@live.com>
2021-02-10 13:33:15 -0800

    Updates

--- BuffOverlayLeft.lua
+++ BuffOverlayLeft.lua
@@ -68,6 +68,11 @@ local prioritySpellList = { --The higher on the list, the higher priority the bu
 203538, --Greater Blessing of Kings
 203539, --Greater Blessing of Wisdom
 288509, --Shadow Resistance Aura
+183435, --Retribution Aura
+317920, --Concentration Aura
+32223, --Crusader Aura
+465, --Devotion Aura
+
 }
 
 for k, v in ipairs(prioritySpellList) do
